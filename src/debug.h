feae6f6e7d8c88a5b870213d856f725222d2352b
PaulOborowski <oborowski.paul@gmail.com>
2021-08-31 17:36:02 +0200

    added debug macro

---
+++ src/debug.h
@@ -0,0 +1,11 @@
+//#define DEBUG_BUILD
+#ifdef DEBUG_BUILD
+#define DEBUG(x)                                                               \
+  do {                                                                         \
+    std::cerr << x;                                                            \
+  } while (0)
+#else
+#define DEBUG(x)                                                               \
+  do {                                                                         \
+  } while (0)
+#endif
